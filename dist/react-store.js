81ed4e2506979df10353653846ab970a39dc93bb
tony <382026180@qq.com>
2017-10-31 14:50:29 +0800

    add react-store

---
+++ dist/react-store.js
@@ -0,0 +1,70 @@
+
+/**
+ * Created by dcpai on 2017/7/4.
+ */
+(function () {
+  var global = window || this;
+  var ENGIE_LOCAL = 'localStorage';
+  var ENGIE_SESSION = 'sessionStorage';
+  var Store = {
+    engine: ENGIE_LOCAL,
+    set local(str) {
+      this.engine = ENGIE_LOCAL;
+      this.sets(str);
+    },
+    get local() {
+      this.engine = ENGIE_LOCAL;
+      return this.gets();
+    },
+    set session(str) {
+      this.engine = ENGIE_SESSION;
+      this.sets(str);
+    },
+    get session() {
+      this.engine = ENGIE_SESSION;
+      return this.gets();
+    },
+    set: function (inKey, inValue) {
+      global[Store.engine].setItem(inKey, inValue);
+    },
+    get: function (inKey) {
+      return  global[Store.engine].getItem(inKey);
+    },
+    sets: function (inObject) {
+      for (var inKey in inObject) {
+        Store.set(inKey, inObject[inKey]);
+      }
+    },
+    gets: function (inKeys) {
+      var result = {};
+      var keys = inKeys || [];
+      var i = 0, key;
+      var storeEngine = global[Store.engine];
+      if (keys.length === 0) {
+        for (i = 0; i < storeEngine.length; i++) {
+          key = storeEngine.key(i);
+          keys.push(key);
+        }
+      }
+      for (var k in keys) {
+        result[keys[k]] = Store.get(keys[k]);
+      }
+      return result;
+    },
+    clear: function (inKey) {
+      global[Store.engine].removeItem(inKey);
+    },
+    clearAll: function (inArray) {
+      if (Array.isArray(inArray)) {
+        inArray.forEach(function (item) {
+          Store.clear(item);
+        })
+      } else {
+        global[Store.engine].clear();
+      }
+    }
+  };
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Store;
+  }
+}());
